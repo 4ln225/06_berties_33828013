7301f2623e202480ad6f6f6901441a878d8509ae
rndm95 <150792210+rndm95@users.noreply.github.com>
2025-12-04 23:39:46 +0000

    up

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,9 +2,9 @@
 
 USE berties_books;
 
--- Add example book
+
 INSERT INTO books (name, price) VALUES ('Brighton Rock', 12.99);
 
--- Add required GOLD user for marking
+
 INSERT INTO users (first, last, username, email, password)
 VALUES ('Gold', 'User', 'gold', 'gold@example.com', '$2b$10$LxrZzLUss3jUl4mRgA1NK.YlLG5cENFHDbdH.wZmmk4bxwMwdk6PO');
